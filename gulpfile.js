6de18a248d6763aacbfd2757e5dd6e557c3ea9f2
Miriam Elizabeth Laguna Velazquez <miriam.elizabeth.lv@gmail.com>
2017-11-13 09:24:35 -0600

    resolviendo problemas

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+var gulp = require("gulp");
+
+var rutas = {
+	rutaAssets: 'public/assets',
+  rutaCSS: 'css/*.css',
+	rutaJS: 'js/*.js',
+	rutaNodeModules: 'node_modules/'
+};
+
+gulp.task("estilos", function(){
+	gulp.src(rutas.rutaNodeModules)
+	.pipe(gulp.dest("public"))
+});
+
+gulp.task("assets", function(){
+	gulp.src('index.html')
+	.pipe(gulp.dest("public"))
+});
+gulp.task("prepararCSS", function(){
+	gulp.src(rutas.rutaCSS)
+	.pipe( gulp.dest(rutas.rutaAssets + "/css"))
+});
+
+gulp.task('prepararJS', function(){
+	gulp.src(rutas.rutaJS)
+	.pipe( gulp.dest(rutas.rutaAssets + "/js"))
+});
+gulp.task('serveprod', function() {
+  connect.server({
+    root: "./public/",
+    port: process.env.PORT || 5000, // localhost:5000
+    livereload: false
+  });
+});
+
+gulp.task("observar", function(){
+	gulp.watch('index.html',['assets']);
+	gulp.watch(rutas.rutaCSS,['prepararCSS']);
+	gulp.watch(rutas.rutasJS,['prepararJS']);
+	gulp.watch(rutas.rutaNodeModules,['assets']);
+	gulp.watch('index.html',['estilos']);
+});
--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.use('/static', express.static(__dirname+'/node_modules'));
-
-app.listen(3000, () => {
-	console.log("Listening on 3000");
-});
